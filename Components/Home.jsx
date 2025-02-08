32a1154af4c5612f3120d24a7d483a4ac2f46bd3
Aayush2883 <160393828+Aayush2883@users.noreply.github.com>
2025-02-08 13:56:44 +0530

    added home page

---
+++ Components/Home.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import logo from './images/BIG_LOGO.png'
+
+
+const Home = () => {
+  return (
+    <div className='flex gap-35 mt-8'>
+        
+    
+        <div className=' flex flex-col  w-[35vw] ml-40 mt-14'>
+          <div>
+          <h1 className='text-[7em]' id='company'>ECLYRA</h1>
+          <p className='text-[1.2em] relative bottom-8.5 left-2' >Get a clean future</p>
+          <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Consequuntur, ut est assumenda quibusdam, error facilis eos optio maiores architecto incidunt adipisci sapiente labore earum amet, soluta repellat ratione laborum placeat!
+          </p>
+          </div>
+          <button class='button mt-15 py-3 self-end mr-5 w-[60%]'><p class="text">Get Started</p></button>
+        </div>
+    
+    
+        <div>
+        <img  src={logo} className='h-[32vw]' />
+        </div>
+        </div>
+  )
+}
+
+export default Home
