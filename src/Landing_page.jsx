f9eee125f587f6db5686b33a5cb34e60ace18ab6
Darth-hub <ayushranjan112400@gmail.com>
2025-03-31 15:09:16 +0530

    All Ok Now

--- src/Landing_page.jsx
+++ src/Landing_page.jsx
@@ -1,20 +1,20 @@
 import React, { useEffect, useState } from 'react'
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import './App.css'
-import Home from '../Components/Home'
-import Blogs from './Pages/Blogs'
-import Campaign from './Pages/Campaign'
-import HeroBanner from '../Components/HeroBanner';
+import Home from '../Components/Home.jsx'
+import Blogs from './Pages/Blogs.jsx'
+import Campaign from './Pages/Campaign.jsx'
+import HeroBanner from '../Components/HeroBanner.jsx';
 import OurPresence from './Pages/OurPresence.jsx';
-import Line from '../Components/Line'
-import Customer_Review from './Pages/Customer_Review'
-import How_We_Work from './Pages/How_We_Work'
-import Footer from '../Components/Footer'
-import User_profile from './Pages/User/User_profile'
-import User_dashboard from './Pages/User/User_dashboard'
-import Buy_scarp from './Pages/User/Buy_scarp'
-import Community from './Pages/Community'
-import Join_community from './Pages/User/Join_community'
+import Line from '../Components/Line.jsx'
+import Customer_Review from './Pages/Customer_Review.jsx'
+import How_We_Work from './Pages/How_We_Work.jsx'
+import Footer from '../Components/Footer.jsx'
+import User_profile from './Pages/User/User_profile.jsx'
+import User_dashboard from './Pages/User/User_dashboard.jsx'
+import Buy_scarp from './Pages/User/Buy_scarp.jsx'
+import Community from './Pages/Community.jsx'
+import Join_community from './Pages/User/Join_community.jsx'
 import Store from './Pages/StorePage.jsx';
 import BlogPage from './Pages/BlogsPage.jsx';
 import TermsCondition from './Pages/TermsCondition.jsx'
