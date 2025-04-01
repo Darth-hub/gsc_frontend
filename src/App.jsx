e48d244dd005ccada289d3be1b4e20910c158743
Darth-hub <ayushranjan112400@gmail.com>
2025-04-02 00:23:38 +0530

    x

--- src/App.jsx
+++ src/App.jsx
@@ -14,7 +14,7 @@ import Expandedblogs from './Pages/Components/Expandedblogs.jsx';
 import Authentication from  '../src/Pages/Authentication.jsx'
 import SchedulePickUp from './Pages/SchedulePickUp.jsx'
 import User_profile from './Pages/User/User_profile.jsx';
-import  ViewPickUp from './Pages/ViewPickUp.jsx'
+import ViewPickUp from './Pages/ViewPickUp.jsx'
 import SellToIndustries from './Pages/SellToIndustries.jsx'
 import ViewIndustries from './Pages/ViewIndustries.jsx';
 import { View } from 'lucide-react';
@@ -30,10 +30,6 @@ function App() {
     <>    
     <Routes>
       <Route path='/' element={<LandingPage />} />
-    </Routes>
-    
-
-    <Routes>
       <Route path='/community' element={<Community />} />
       <Route path="/community/:communityName" element={<Join_community />} />
       <Route path='/User' element={<User_profile />} />
@@ -47,7 +43,7 @@ function App() {
       <Route path='/schedulepickup' element={<SchedulePickUp/>} />
       <Route path='/viewPickUps' element={<ViewPickUp/>} />
       <Route path='/viewindustries' element={<ViewIndustries/>} />
-      <Route path='selltoindustries/:id' element={<SellToIndustries/>} />
+      <Route path='/selltoindustries/:id' element={<SellToIndustries/>} />
       <Route path= '/CommunityChat' element={<CommunityChat/>} />
     </Routes>
     
